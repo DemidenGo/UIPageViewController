4ee387df191af73540be4fe93eb0ab84108aa7e0
Demidengo <dimidengo@yandex.ru>
2023-04-30 14:32:07 +0300

    Cyclic display of controllers added

--- UIPageViewController/OnboardingViewController.swift
+++ UIPageViewController/OnboardingViewController.swift
@@ -58,15 +58,21 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
         let previousIndex = viewControllerIndex - 1
-        guard previousIndex >= 0 else { return nil }
-        return pages[previousIndex]
+        if previousIndex >= 0 {
+            return pages[previousIndex]
+        } else {
+            return pages.last
+        }
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
         let nextIndex = viewControllerIndex + 1
-        guard nextIndex < pages.count else { return nil }
-        return pages[nextIndex]
+        if nextIndex < pages.count {
+            return pages[nextIndex]
+        } else {
+            return pages.first
+        }
     }
 
     //    собственный иникатор текущей страницы UIPageViewController'a
